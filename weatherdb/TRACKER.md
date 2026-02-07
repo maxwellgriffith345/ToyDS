04e1ed3295e8c8d09740057ca17b5605a451866a
Maxwell Griffith <maxwellgriffith345@gmail.com>
2026-02-07 12:04:36 -0600

    got the automation to work

--- weatherdb/TRACKER.md
+++ weatherdb/TRACKER.md
@@ -3,14 +3,17 @@
 - started a compose file to use to build the container
 - open psql terminal with in the contrainer to work with the database
 
-## Last Update 2/4
+## Last Update 2/5
 - tied the schema.sql file to the init-db for the container so it builds the db on start up
+- command to load data from csv file works
 - trying to get the bash script to work for loading the data, running into an issue where the db is not ready to accept input before calling the load command?
+
+## Last Update 2/7
+- used pg_ready to check the status of the connection before loading db
+- I think there is an issue with the db restarting
+- the bash script works to atuomate building the contrainer and the data Database
+- CALLING IT DONE: Minimual vialbe product
+
 
 ## NEXT STEP
 - load in the weather events data using bash script
-
-## NEXT NEXT STEP
-- figure out how to automate it all
-  - compose for the docker file
-  - bash file for all the other commands?
--- weatherdb/builddb.sh
+++ weatherdb/builddb.sh
@@ -5,6 +5,34 @@ echo "Building Weather Database"
 # build the container
 docker compose up -d
 
+# need to use wait for sleep here to wait until db is fully built
+# is there a way to wait until the port/socket is ready?
+
+
+# use pg_ready to wait to load data
+until docker exec weathercnt pg_isready -U myuser -d weatherdb; do
+  sleep 1
+done
+
+sleep 5
+wait
+
+echo "Data base Built"
+echo "Loading Data"
+
+
+
+# load the csv file into the db
+docker exec -i weathercnt \
+    psql -U myuser -d weatherdb \
+    -c "\copy weather_events FROM STDIN WITH (FORMAT csv, HEADER true)" \
+    < ./rawdata/stormevents.csv
+
+echo "Data load finished"
+
+# tear down contrainer
+# docker compose down -v
+
 # access the database
 # docker exec -it container_name psql -U username dbname
 # docker exec: runs a command inside a container
@@ -13,17 +41,3 @@ docker compose up -d
 
 # get logs for the container
 # docker logs -f container_name
-
-# need to use wait for sleep here to wait until db is fully built
-
-echo "Data base Built"
-echo "Loading Data"
-
-# load the csv file into the db
-docker exec -i weathercnt \
-    psql -U myuser -d weatherdb \
-    -c "\copy weather_events FROM STDIN WITH (FORMAT csv, HEADER true)" \
-    < ./rawdata/stormevents.csv
-
-
-echo "Data load finished"
