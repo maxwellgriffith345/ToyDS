bd6b03c43a70cd7d749e8af9ae9eb57a9d37fd35
Maxwell Griffith <maxwellgriffith345@gmail.com>
2026-02-14 12:58:47 -0600

    model training and saving work

--- irisAPI/model_training.py
+++ irisAPI/model_training.py
@@ -16,11 +16,14 @@ X,y = iris['data'],iris['target']
 X_train, X_test, y_train, y_test = train_test_split(X, y,
                                     test_size = 0.10, random_state = 42)
 
-#export the test data?
+#export the test data
+M_test = X_test.merge(y_test, left_index = True, right_index = True)
+M_test.to_csv("data/Mtestdata.csv", index=False)
+X_test.to_csv("data/Xtestdata.csv", index=False)
 
 #Create pipeline the inlcudes data preprocessing steps and model
+# Don't have any pre-preprocessing steps
 pipeline = Pipeline([
-    ('preprocessing', 'INSERT HERE'),
     ('model', tree.DecisionTreeClassifier())
 ])
 
@@ -28,12 +31,14 @@ pipeline = Pipeline([
 pipeline.fit(X_train, y_train)
 
 #save the pipeline
-joblib.dump(pipeline, 'FILE PATH')
+# Use 'with' as a context manager
+with open('model/irisdecisiontree.joblib', 'wb') as file:
+    joblib.dump(pipeline, file)
 
-
+print("model saved")
 #Load the pipeline back in (would use in seperate file)
-loaded_pipeline = joblib.load('FILE PATH')
-predictions = loaded_pipeline.predict(new_data)
+#loaded_pipeline = joblib.load('FILE PATH')
+#predictions = loaded_pipeline.predict(new_data)
 
 """
 # train the model
